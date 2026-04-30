1d5f7a9673e1c0ca9995d2b461b7e86b4c0aa969
gow313 <gowthamrlmc@gmail.com>
2026-04-30 22:05:14 +0530

    cahnges

--- src/app/components/Projects/ProjectCard.tsx
+++ src/app/components/Projects/ProjectCard.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 
-import { MotionDiv, MotionNav } from '../../lib/motion'
+import { MotionDiv } from '../../lib/motion'
 import { projectType } from '../../utils/data'
 
 import { Code2, ExternalLink } from 'lucide-react'
@@ -9,27 +9,20 @@ type Props = {
   projectData: projectType
 }
 
-const item = {
-  initial: {
-    opacity: 0,
-    y: 24
-  },
-  animate: {
-    opacity: 1,
-    y: 0
-  }
-}
-
 export const ProjectCard = ({ projectData }: Props) => {
   return (
     <MotionDiv className="relative flex flex-col gap-3 sm:max-w-sm">
+      
       <div className="relative flex-col gap-4">
         <Image
           src={projectData.image}
           alt={`${projectData.name} image`}
           width={448}
           height={280}
+          unoptimized   
+          className="w-full h-auto"
         />
+
         <MotionDiv
           initial="initial"
           animate="initial"
@@ -38,21 +31,24 @@ export const ProjectCard = ({ projectData }: Props) => {
         >
         </MotionDiv>
 
-      </div >
+      </div>
+
       <div className="flex flex-col gap-4">
         <h3 className="text-2xl font-bold">{projectData.name}</h3>
         <p className="text-lg">{projectData.description}</p>
+
         <div className="flex flex-wrap gap-3">
           {projectData.tags.map((tag, index) => (
             <span
-              className="rounded-3xl bg-target bg-opacity-20 px-2 py-1 text-sm font-bold text-target "
               key={index}
+              className="rounded-3xl bg-target bg-opacity-20 px-2 py-1 text-sm font-bold text-target"
             >
               {tag}
             </span>
           ))}
         </div>
       </div>
-    </MotionDiv >
+
+    </MotionDiv>
   )
 }
